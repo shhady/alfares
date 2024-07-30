bbe056e03a0f0004bb6d9d6ec774b97df03295af
shhady <shhadyse@gmail.com>
2024-07-30 15:05:14 +0300

    delete clgs

--- app/(pages)/properties/page.jsx
+++ app/(pages)/properties/page.jsx
@@ -1,4 +1,5 @@
 import ClientComponent from '@/components/new-properties/ClientComponent';
+import React, { Suspense } from 'react';
 
 export default async function NewProperties() {
   const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROD_URL ? process.env.NEXT_PUBLIC_BACKEND_PROD_URL : process.env.NEXT_PUBLIC_BACKEND_DEV_URL }/api/properties/get-properties`,
@@ -11,13 +12,16 @@ export default async function NewProperties() {
   const data = await response.json();
 
   return (
+    <Suspense fallback={<div>Loading...</div>}>
     <div className='bg-white text-black min-h-screen'>
       <div className='p-8 text-center flex flex-col gap-4 max-w-screen-2xl m-auto'>
         <h1 className='text-4xl'>احدث العقارات</h1>
         <div className='border-b-2 md:w-80 w-40 mx-auto'></div>
         <p className=''>اطّلع على أحدث العقارات المعروضة في الإمارات</p>
         <ClientComponent data={data} />
+
       </div>
     </div>
+    </Suspense>
   );
 }
