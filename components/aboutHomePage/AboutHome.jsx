1d8ec16beec6e107007588b99c106d205c73c415
shhady <shhadyse@gmail.com>
2024-08-01 22:24:08 +0300

    animating about home page

--- components/aboutHomePage/AboutHome.jsx
+++ components/aboutHomePage/AboutHome.jsx
@@ -1,21 +1,23 @@
+"use client";
 import React from 'react'
+import { TextGenerateEffect } from "../ui/text-generate-effect";
 
 export default function AboutHome() {
   return (
-    <div className='bg-[#191919] text-white flex'>
-      <div className=' h-full xl:flex gap-4 xl:gap-8 justify-center items-center  max-w-screen-2xl m-auto'>
+    <div className='bg-[#191919] text-white flex md:h-[80vh]'>
+      <div className='pb-8  xl:flex gap-4 xl:gap-8 justify-center items-center  max-w-screen-2xl m-auto'>
         <div className='flex flex-col gap-3 justify-start items-start p-8'>
-          <h1 className='text-3xl font-bold text-center mb-8'>عدي فارس - خبير الاستثمار العقاري في دبي</h1>
-          <h2 className='text-2xl font-medium'>خبرة موثوقة، نتائج متميزة</h2>
+          <h1 className='text-3xl font-bold text-center mb-8 md:text-start md:w-fit w-full line-under-white'>عدي فارس <br/> خبير الاستثمار العقاري في دبي</h1>
+          <h2 className='text-3xl font-medium text-[#9e8636]'>خبرة موثوقة، نتائج متميزة</h2>
 
-<p>
-أنا عدي فارس، خبير في مجال الاستثمار العقاري بدبي، نشأت في أسرة ذات جذور عميقة في التطوير العقاري. منذ أن كنت صغيرًا، تعلمت من والدي الذي كان مطورًا عقاريًا بارزًا قام ببناء عقارات في عكا واستثمر في عقارات دبي. اليوم، أستفيد من هذه الخبرة الغنية لمساعدة الأفراد على اتخاذ قرارات استثمارية مدروسة وناجحة في سوق العقارات بدبي.
-</p>  
+          <TextGenerateEffect duration={2} filter={false} words={'أنا عدي فارس، خبير في مجال الاستثمار العقاري بدبي، نشأت في أسرة ذات جذور عميقة في التطوير العقاري. منذ أن كنت صغيرًا، تعلمت من والدي الذي كان مطورًا عقاريًا بارزًا قام ببناء عقارات في عكا واستثمر في عقارات دبي. اليوم، أستفيد من هذه الخبرة الغنية لمساعدة الأفراد على اتخاذ قرارات استثمارية مدروسة وناجحة في سوق العقارات بدبي.'} />
+ 
 
-    <h2 className='text-2xl font-medium'>استشارات الاستثمار العقاري</h2>
-    <p>أقدم خدمات استشارية متخصصة في الاستثمار العقاري، مصممة خصيصًا لمساعدتك على اتخاذ القرارات الصحيحة عند الاستثمار في عقارات دبي. بفضل خبرتي الطويلة وفهمي العميق للسوق، أضمن تقديم نصائح استراتيجية تحقق لك العائد الأمثل على استثماراتك.
+    <h2 className='text-3xl font-medium text-[#9e8636]'>استشارات الاستثمار العقاري</h2>
+              <TextGenerateEffect duration={2} filter={false} words={'أقدم خدمات استشارية متخصصة في الاستثمار العقاري، مصممة خصيصًا لمساعدتك على اتخاذ القرارات الصحيحة عند الاستثمار في عقارات دبي. بفضل خبرتي الطويلة وفهمي العميق للسوق، أضمن تقديم نصائح استراتيجية تحقق لك العائد الأمثل على استثماراتك.'} />
 
-</p>
+
+
       </div>
         <div className='flex justify-center items-center lg:p-8'>
         <iframe 
