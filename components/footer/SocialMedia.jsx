88fe445f7f295b6e623319a35b6e3cf8ef8ba26c
shhady <shhadyse@gmail.com>
2024-08-01 22:37:28 +0300

    animating about home page

--- components/footer/SocialMedia.jsx
+++ components/footer/SocialMedia.jsx
@@ -1,6 +1,7 @@
 'use client'
 import React from 'react';
 import { Phone, Mail, Linkedin, Instagram, Facebook } from 'lucide-react';
+import { FaFacebook, FaInstagram,  FaWhatsapp,FaPhone } from "react-icons/fa"
 import "./social.css";
 import Link from 'next/link';
 export default function SocialMedia() {
@@ -13,21 +14,17 @@ export default function SocialMedia() {
     <div>
     <h1 className='text-[1rem] text-start mb-4 hidden md:block border-b-2 border-white py-2'>تواصل</h1>
     <div className="flex justify-center items-center gap-4">
-      <Phone className="iconPhone" onClick={() => window.open("tel:0543113297", "_blank")} />
+      <FaPhone className="iconPhone" onClick={() => window.open("tel:0543113297", "_blank")} />
       <Mail className="iconEmail" onClick={sendEmail} />
       <Link href="https://api.whatsapp.com/send?phone=972543113297" target="_blank" rel="noreferrer">
-        <div className='bg-green-500 rounded-lg w-24 h-16 flex justify-center items-center'>
-            <div className='rounded-full border-2 border-white w-12 h-12 flex justify-center items-center'>
-            <Phone />
-            </div>
-
-        </div>
+            <FaWhatsapp className="iconFacebook" />
+        
       </Link>
       <Link href="https://www.instagram.com/shhadyse/" target="_blank" rel="noreferrer">
-        <Instagram />
+        <FaInstagram className="iconFacebook" />
       </Link>
       <Link href="https://www.facebook.com/shhady.serhan/" target="_blank" rel="noreferrer">
-        <Facebook className="iconFacebook" />
+        <FaFacebook className="iconFacebook" />
       </Link>
     </div>
     </div>
