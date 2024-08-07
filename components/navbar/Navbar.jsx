cc144d42543e0891d72aa9482a8b97f20b51d0e5
shhady <shhadyse@gmail.com>
2024-08-07 13:47:53 +0300

    nav mobile

--- components/navbar/Navbar.jsx
+++ components/navbar/Navbar.jsx
@@ -65,39 +65,46 @@ export default function Navbar() {
             <div className="lg:hidden absolute right-4" onClick={toggleMenu}>
                 <Menu className='text-white'/>
             </div>
-            <div className={`absolute top-24 left-0 right-0 bg-[#303030] lg:hidden ${openMenu ? 'flex' : 'hidden'} flex-col items-center gap-6 py-4`}>
-                <ul className="flex flex-col items-center gap-4">
-                    <Link href='/' onClick={toggleMenu}>
-                        <li className={isActive('/')}>الرئيسية</li>
-                    </Link>
-                    <Link href='/properties' onClick={toggleMenu}>
-                        <li className={isActive('/properties')}>عقارات دبي</li>
-                    </Link>
-                    <Link href='/contact' onClick={toggleMenu}>
-                        <li className={isActive('/contact')}>تواصل معي</li>
-                    </Link>
-                    <Link href='/blog' onClick={toggleMenu}>
-                        <li className={isActive('/blog')}>مقالات</li>
-                    </Link>
-                    <Link href='/about' onClick={toggleMenu}>
-                        <li className={isActive('/about')}>من نحن</li>
-                    </Link>
-                    {isSignedIn && <SignOutButton className="text-white"/>}
-                    <div className='flex gap-6'>
-                    <Link href="https://api.whatsapp.com/send?phone=972542464585" target="_blank" rel="noreferrer">
-                     <FaWhatsapp className="iconFacebook" />
-        
-                     </Link>
-                      <Link href="https://www.instagram.com/ady_alfares/" target="_blank" rel="noreferrer">
-                      <FaInstagram className="iconFacebook" />
-                      </Link>
-                     <Link href="https://www.facebook.com/profile.php?id=100064084111341" target="_blank" rel="noreferrer">
-                      <FaFacebook className="iconFacebook" />
-                     </Link>
-                     </div>
-                    
-                </ul>
-            </div>
+            
+
+             {/* Backdrop Div */}
+      {openMenu && (
+        <div className="fixed inset-0 top-24 bg-black opacity-50 z-10" onClick={toggleMenu}></div>
+      )}
+
+      {/* Menu Div */}
+      <div className={`fixed top-24 left-0 right-0 bg-[#303030] lg:hidden z-20 transition-transform duration-300 ease-in-out ${openMenu ? 'flex' : 'hidden'} flex-col items-center gap-6 py-4 ${openMenu ? 'transform translate-y-0 opacity-100' : 'transform -translate-y-full opacity-0'}`}>
+        <ul className="flex flex-col items-center gap-4">
+          <Link href='/' onClick={toggleMenu}>
+            <li className={isActive('/')}>الرئيسية</li>
+          </Link>
+          <Link href='/properties' onClick={toggleMenu}>
+            <li className={isActive('/properties')}>عقارات دبي</li>
+          </Link>
+          <Link href='/contact' onClick={toggleMenu}>
+            <li className={isActive('/contact')}>تواصل معي</li>
+          </Link>
+          <Link href='/blog' onClick={toggleMenu}>
+            <li className={isActive('/blog')}>مقالات</li>
+          </Link>
+          <Link href='/about' onClick={toggleMenu}>
+            <li className={isActive('/about')}>من نحن</li>
+          </Link>
+          <div className='flex gap-6'>
+          <FaPhone className="iconPhone" onClick={() => window.open("tel:0542464585", "_blank")} />
+            <Link href="https://api.whatsapp.com/send?phone=972542464585" target="_blank" rel="noreferrer">
+              <FaWhatsapp className="iconFacebook" />
+            </Link>
+            <Link href="https://www.instagram.com/ady_alfares/" target="_blank" rel="noreferrer">
+              <FaInstagram className="iconFacebook" />
+            </Link>
+            <Link href="https://www.facebook.com/profile.php?id=100064084111341" target="_blank" rel="noreferrer">
+              <FaFacebook className="iconFacebook" />
+            </Link>
+          </div>
+          {isSignedIn && <SignOutButton className="text-white" />}
+        </ul>
+      </div>
         </div>
     )
 }
