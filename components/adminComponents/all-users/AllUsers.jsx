8a58d1872ac35ce7a199043c9d3e50f09fa120bc
shhady <shhadyse@gmail.com>
2024-08-14 23:03:12 +0300

    replace icon8 with lucide

--- components/adminComponents/all-users/AllUsers.jsx
+++ components/adminComponents/all-users/AllUsers.jsx
@@ -8,10 +8,10 @@ export default function AllUsers() {
   const [data, setData] = useState([]);
   const [filter, setFilter] = useState('');
   const [selectedUser, setSelectedUser] = useState(null); // State to track selected user
-
+console.log(data);
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch('/api/users/get-users');
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROD_URL ? process.env.NEXT_PUBLIC_BACKEND_PROD_URL : process.env.NEXT_PUBLIC_BACKEND_DEV_URL}/api/users/get-users`);
       if (!response.ok) {
         return <div className='mt-8 flex justify-center items-center'>No users found</div>;
       }
