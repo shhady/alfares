453db3c182037aaa84f4794f05357977a5f72d6a
shhady <shhadyse@gmail.com>
2024-08-14 16:52:59 +0300

    blog became client side rendering

--- app/(pages)/blog/page.jsx
+++ app/(pages)/blog/page.jsx
@@ -1,14 +1,20 @@
 
+'use client'
 import BlogList from '@/components/blog-list/BlogList'
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 
-export default async function page() {
-  const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROD_URL ? process.env.NEXT_PUBLIC_BACKEND_PROD_URL : process.env.NEXT_PUBLIC_BACKEND_DEV_URL }/api/blogs/get-blogs`,
-  { next: { revalidate: 360 } });
-  if (!response.ok) {
-    return <div className='mt-8 flex justify-center items-center'>No products found</div>;;
-  }
-  const data = await response.json();
+export default function page() {
+  const [data, setData ] = useState([])
+  useEffect(()=>{
+    const fetchData = async ()=>{
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_PROD_URL ? process.env.NEXT_PUBLIC_BACKEND_PROD_URL : process.env.NEXT_PUBLIC_BACKEND_DEV_URL }/api/blogs/get-blogs`)
+      const data = await response.json();
+      setData(data);
+    }
+    fetchData()
+    
+  },[])
+
   return (
     <div >
         <BlogList data={data}/>
