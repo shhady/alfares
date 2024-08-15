1a1b4964ade03c7900156b45a4a73df19b200ed8
shhady <shhadyse@gmail.com>
2024-08-15 22:05:52 +0300

    added headers for cors

--- next.config.mjs
+++ next.config.mjs
@@ -39,6 +39,10 @@ const nextConfig = {
               key: 'Permissions-Policy',
               value: 'geolocation=(self), microphone=(self), camera=(self)', // Use only recognized features
             },
+            { key: "Access-Control-Allow-Credentials", value: "true" },
+            { key: "Access-Control-Allow-Origin", value: "*" },
+            { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+            { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
           ],
         },
       ];
