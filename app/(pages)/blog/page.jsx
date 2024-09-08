2595e2d6f7a2892052320612a8e5a3e6663f3669
shhady <shhadyse@gmail.com>
2024-09-08 13:56:15 +0300

    articles

--- app/(pages)/blog/page.jsx
+++ app/(pages)/blog/page.jsx
@@ -5,6 +5,7 @@ const articles = [
     source: 'AlMadar Magazine',
     title: 'عدي فارس "نسر العقارات" يحلق في عالم الاستثمار العقاري',
     image: '/blog-2.png', // Replace with correct image
+    dir:'rtl',
     excerpt: 'عدي فارس خبير في مجال الاستثمار العقاري...',
     link: 'https://www.almadarmagazine.ae/2024/08/23/%D8%B9%D8%AF%D9%8A-%D9%81%D8%A7%D8%B1%D8%B3-%D9%86%D8%B3%D8%B1-%D8%A7%D9%84%D8%B9%D9%82%D8%A7%D8%B1%D8%A7%D8%AA-%D9%8A%D8%AD%D9%84%D9%82-%D9%81%D9%8A-%D8%B9%D8%A7%D9%84%D9%85-%D8%A7%D9%84%D8%A7/',
   },
@@ -12,6 +13,7 @@ const articles = [
     source: 'UAE News247',
     title: 'Ady Fares, “The Eagle of Real Estate” soars in the world of real estate investment',
     image: '/blog-1.png', // Replace with correct image
+    dir:'ltr',
     excerpt: 'Ady Fares is an expert in the field of real estate investment...',
     link: 'https://uaenews247.com/2024/08/25/ady-fares-the-eagle-of-real-estate-soars-in-the-world-of-real-estate-investment',
   },
@@ -19,6 +21,7 @@ const articles = [
     source: 'UAE News 4U',
     title: 'Ady Fares, “The Eagle of Real Estate” soars in the world of real estate investment',
     image: '/blog-1.png', // Replace with correct image
+    dir:'ltr',
     excerpt: 'Ady Fares is a real estate expert with deep roots in the industry...',
     link: 'https://uaenews4u.com/2024/08/25/ady-fares-the-eagle-of-real-estate-soars-in-the-world-of-real-estate-investment',
   },
@@ -26,6 +29,7 @@ const articles = [
     source: 'Merimedia',
     title: 'عدي فارس "نسر العقارات" يقدم خدمات استشارية متخصصة',
     image: '/blog-3.png', // Replace with correct image
+    dir:'rtl',
     excerpt: 'يتمتع عدي فارس بخبرة عميقة في مجال الاستثمار العقاري...',
     link: 'https://www.merimedia.net/2024/08/27/%d8%b9%d8%af%d9%8a-%d9%81%d8%a7%d8%b1%d8%b3-%d9%86%d8%b3%d8%b1-%d8%a7%d9%84%d8%b9%d9%82%d8%a7%d8%b1%d8%a7%d8%aa-%d9%8a%d9%82%d8%af%d9%85-%d8%ae%d8%af%d9%85%d8%a7%d8%aa-%d8%a7%d8%b3%d8%aa%d8%b4/ ',
   },
@@ -33,6 +37,7 @@ const articles = [
     source: 'Merimedia (EN)',
     title: 'Ady Faris, “Eagle of Real Estate” Provides Specialized Consulting Services in Real Estate Investment',
     image: '/blog-3.png', // Replace with correct image
+    dir:'ltr',
     excerpt: 'Ady Fares, known as the "Eagle of Real Estate," continues to offer...',
     link: 'https://www.merimedia.net/en/2024/08/27/ady-faris-eagle-of-real-estate-provides-specialized-consulting-services-in-real-estate-investment/',
   },
@@ -42,12 +47,13 @@ const articles = [
     image: '/blog-3.png', // Replace with correct image
     excerpt: 'Ady Fares continues to establish himself in the real estate sector...',
     link: 'https://pardaisnews.com/?p=11610',
+    dir:'ltr',
   }
 ];
 
 const ArticleCard = ({ article }) => {
   return (
-    <div className="max-w-xs rounded overflow-hidden shadow-lg mx-4 my-6">
+    <div className="max-w-xs rounded overflow-hidden shadow-lg mx-4 my-6" dir={article.dir}>
       <a href={article.link} target="_blank" rel="noopener noreferrer">
         <img
           className="w-full h-48 object-cover"
